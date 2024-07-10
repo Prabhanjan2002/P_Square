ddca1a74a9e82eba0f0aca906f1a11aa3972f934
Prabhanjan Mishra <prabhanjanmishra60077@gmail.com>
2024-07-10 15:07:48 +0530

    created user post like button , how much likes and replies etc

--- frontend/src/pages/UserPage.jsx
+++ frontend/src/pages/UserPage.jsx
@@ -1,9 +1,34 @@
 import UserHeader from "../components/UserHeader";
+import UserPost from "../components/UserPost";
 
 const UserPage = () => {
   return (
     <>
-      <UserHeader />
+      <UserHeader
+        likes={1200}
+        replies={481}
+        postImg="/post1.png"
+        postTitle="lets talk"
+      />
+      <UserPost
+        likes={1200}
+        replies={481}
+        postImg="/post1.png"
+        postTitle="lets talk"
+      />
+      <UserPost
+        likes={1200}
+        replies={481}
+        postImg="/post2.png"
+        postTitle="lets talk"
+      />
+      <UserPost
+        likes={1200}
+        replies={481}
+        postImg="/post1.png"
+        postTitle="lets talk"
+      />
+      <UserPost likes={1200} replies={481} postTitle="post without picture" />
     </>
   );
 };
