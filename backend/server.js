bd8bab10d4786a6eab3c99a45be445aa96f92d1b
Prabhanjan Mishra <prabhanjanmishra60077@gmail.com>
2024-07-12 02:33:37 +0530

    connected to mongodb database successfully

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./db/connectDB.js";
+
+dotenv.config();
+connectDB();
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(5000, () => console.log(`server started at port ${PORT}`));
