824d006c9898b4b2208c8c5f0b5b3497e8ed2d3c
Prabhanjan Mishra <prabhanjanmishra60077@gmail.com>
2024-07-14 03:07:57 +0530

    all the user functionalities created on backend :- getusers,signup,login,logout,follow/unfllow,updateuser

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,20 @@ import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
 import generateTokenAndSetCookies from "../utils/helpers/generateTokenAndSetCookies.js";
 
+const getUserProfile = async (req, res) => {
+  const { username } = req.params;
+  try {
+    const user = await User.findOne({ username })
+      .select("-password")
+      .select("-updatedAt");
+    if (!user) return res.status(400).json({ message: "user not found" });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+    console.log("Error on getUserProfile", error.message);
+  }
+};
+
 const signupUser = async (req, res) => {
   try {
     const { name, email, username, password } = req.body;
@@ -70,4 +84,75 @@ const logoutUser = async (req, res) => {
   }
 };
 
-export { signupUser, loginUser, logoutUser };
+const followUnFollowUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const userToModify = await User.findById(id);
+    const currentUser = await User.findById(req.user._id);
+
+    if (id === req.user._id.toString())
+      return res
+        .status(400)
+        .json({ error: "You cannot follow/unfollow yourself" });
+
+    if (!userToModify || !currentUser)
+      return res.status(400).json({ error: "User not found" });
+
+    const isFollowing = currentUser.following.includes(id);
+
+    if (isFollowing) {
+      // Unfollow user
+      await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
+      await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } });
+      res.status(200).json({ message: "User unfollowed successfully" });
+    } else {
+      // Follow user
+      await User.findByIdAndUpdate(id, { $push: { followers: req.user._id } });
+      await User.findByIdAndUpdate(req.user._id, { $push: { following: id } });
+      res.status(200).json({ message: "User followed successfully" });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+    console.log("Error in followUnFollowUser: ", err.message);
+  }
+};
+
+const updateUser = async (req, res) => {
+  const { name, email, username, password, profilePic, bio } = req.body;
+  const userId = req.user._id;
+  try {
+    let user = await User.findById(userId);
+    if (!user) return res.status(400).json({ message: "user not found" });
+
+    if (req.params.id != userId.toString())
+      return res
+        .status(400)
+        .json({ message: "you cant update others profile" });
+
+    if (password) {
+      const salt = await bcrypt.genSalt(10);
+      const hashedpassword = await bcrypt.hash(password, salt);
+      user.password = hashedpassword;
+    }
+    user.name = name || user.name;
+    user.email = email || user.email;
+    user.username = username || user.username;
+    user.profilePic = profilePic || user.profilePic;
+    user.bio = bio || user.bio;
+
+    user = await user.save();
+    res.status(200).json({ message: "profile updated successfully", user });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+    console.log("Error in the updateUser", error.message);
+  }
+};
+
+export {
+  signupUser,
+  loginUser,
+  logoutUser,
+  followUnFollowUser,
+  updateUser,
+  getUserProfile,
+};
